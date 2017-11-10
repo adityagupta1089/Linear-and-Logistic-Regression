bbbaa35ac617a8d7d80dea182e2ad8bfaff8114f
Aditya <adityagupta1089@gmail.com>
2017-11-10 18:20:49 +0530

    .

--- Linear Ridge Regression/meansquarederr.m
+++
@@ -1,4 +0,0 @@
-function err = meansquarederr(T, Tdash)
-err = mean((T-Tdash).^2);
-end
-
--- Linear Ridge Regression/readData.m
+++
@@ -1,5 +0,0 @@
-function A = readData()
-A = readtable('linregdata','ReadVariableNames',false);
-present = @(x) ismember(table2array(A(:,1)), x);
-A = [present('F') present('I') present('M') table2array(A(:,2:end))];
-end
